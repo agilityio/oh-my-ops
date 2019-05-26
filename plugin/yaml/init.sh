366f881d161f9eef010b9084d2d2e83c1e1f1858
Trung Ngo <trungngo@asnet.com.vn>
2019-05-26 10:37:56 +0700

    add yml plugin

---
+++ plugin/yaml/init.sh
@@ -0,0 +1,22 @@
+_do_plugin "yaml"
+
+_do_log_level_debug "yaml"
+_do_src_include_others_same_dir
+
+
+# ==============================================================================
+# Plugin Init
+# ==============================================================================
+
+
+# Initializes yaml plugin.
+#
+function _do_yaml_plugin_init() {
+    if ! _do_alias_feature_check "yaml" "sed" "awk"; then 
+        return 
+    fi 
+
+    _do_src_include_others_same_dir
+
+    _do_log_info "yaml" "Initialize plugin"
+}
---
+++ test/plugin/yaml/util-test.sh
@@ -0,0 +1,48 @@
+
+function test_parse() {
+    local dir="$(_do_src_dir)"
+
+    # Counts the result and make sure there is more than one line
+    local count=$(_do_yaml_parse "${dir}/data-1.yml" | grep "SampleProject_" | wc -l) 
+    [ ${count} -gt 0 ] || _do_assert_fail
+
+    # Counts the result and make sure there is more than one line
+    count=$(_do_yaml_parse "${dir}/data-1.yml" "S_" | grep "SampleProject_" | wc -l) 
+    [ ${count} -gt 0 ] || _do_assert_fail
+}
+
+
+function test_export_uppercase_var() {
+    local dir="$(_do_src_dir)"
+
+    _do_yaml_export_uppercase_var "${dir}/data-1.yml"
+
+    # Counts the result and make sure there is more than one line
+    local count=$(declare -p | grep SAMPLEPROJECT | wc -l) 
+    [ ${count} -gt 0 ] || _do_assert_fail
+
+
+    _do_yaml_export_uppercase_var "${dir}/data-1.yml" "S_"
+
+    # Counts the result and make sure there is more than one line
+    local count=$(declare -p | grep S_SAMPLEPROJECT | wc -l) 
+    [ ${count} -gt 0 ] || _do_assert_fail
+
+}
+
+function test_export_lowercase_var() {
+    local dir="$(_do_src_dir)"
+
+    _do_yaml_export_lowercase_var "${dir}/data-1.yml"
+
+    # Counts the result and make sure there is more than one line
+    local count=$(declare -p | grep sampleproject | wc -l) 
+    [ ${count} -gt 0 ] || _do_assert_fail
+
+    _do_yaml_export_lowercase_var "${dir}/data-1.yml" "s_"
+
+    # Counts the result and make sure there is more than one line
+    local count=$(declare -p | grep s_sampleproject | wc -l) 
+    [ ${count} -gt 0 ] || _do_assert_fail
+
+}
