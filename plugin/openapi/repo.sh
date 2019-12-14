c8b00a810797fefd0de4fd0e399d1dfac7675db7
Trung Ngo <trungngo@asnet.com.vn>
2019-12-14 19:50:10 +0700

    Start add gradle support

--- plugin/openapi/repo.sh
+++ plugin/openapi/repo.sh
@@ -15,5 +15,15 @@ function _do_openapi_angular_client() {
   shift 3
 
   _do_repo_plugin_cmd_add "${repo}" 'openapi' 'gen' 'help'
-  _do_repo_plugin_cmd_opts "${repo}" 'openapi' 'gen' 'typescript-angular '"${config_file}" "${schema_file}"
+  _do_repo_plugin_cmd_opts "${repo}" 'openapi' 'gen' 'typescript-angular' "${config_file}" "${schema_file}"
+}
+
+function _do_openapi_kotlin_client() {
+  local repo=${1?'repo arg required'}
+  local config_file=${2:-}
+  local schema_file=${3:-}
+  shift 3
+
+  _do_repo_plugin_cmd_add "${repo}" 'openapi' 'gen' 'help'
+  _do_repo_plugin_cmd_opts "${repo}" 'openapi' 'gen' 'kotlin' "${config_file}" "${schema_file}"
 }
